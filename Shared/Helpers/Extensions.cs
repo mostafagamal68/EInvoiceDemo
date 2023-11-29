6abaa21bdda20ddcc705ba610849212a9fdc6dec
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2023-11-29 20:29:54 +0200

    more responsive css style.

--- Shared/Helpers/Extensions.cs
+++ Shared/Helpers/Extensions.cs
@@ -1,9 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using System.Text;
+
 namespace EInvoiceDemo.Shared.Helpers;
 
 public static class Extensions
 {
+    public static string? ToString(this object Value, int Format)
+    {
+        if (Value.GetType() == typeof(decimal))
+        {
+            if ((decimal)Value == 0M)
+                return "0";
+            StringBuilder stringBuilder = new();
+            for (int i = 0; i < new string[Format].Length; i++)
+                stringBuilder.Append('0');
+            return string.Format($"{{0:#.{stringBuilder}}}", Value);
+        }
+        return Value.ToString();
+    }
+
     public static bool HasValue(this string? Value)
         => !string.IsNullOrEmpty(Value) && !string.IsNullOrWhiteSpace(Value);
 
@@ -16,7 +32,7 @@ public static class Extensions
            source.OrderBy(c => EF.Property<object>(c, Field))
            :
            source.OrderByDescending(c => EF.Property<object>(c, Field))
-           : 
+           :
            source;
 
     public static T CloneJson<T>(this T source)

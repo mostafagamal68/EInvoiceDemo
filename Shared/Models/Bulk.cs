c54845956105e135ca7710e1c8586c487a2e3493
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2024-03-05 20:44:11 +0200

    Add BulkOperation Dto.

---
+++ Shared/Models/Bulk.cs
@@ -0,0 +1,12 @@
+﻿namespace EInvoiceDemo.Shared.Models;
+
+public class Bulk
+{
+    public List<Guid>? Guids { get; set; }
+    public BulkOperation BulkOperation { get; set; }
+}
+public enum BulkOperation
+{
+    Update,
+    Delete,
+}

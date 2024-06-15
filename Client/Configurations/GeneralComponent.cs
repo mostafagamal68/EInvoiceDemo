3c7627296d2acbc7868d9f125b45c1c65d3fb5c4
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2024-06-15 19:35:37 +0300

    Enhance Components

--- Client/Configurations/GeneralComponent.cs
+++ Client/Configurations/GeneralComponent.cs
@@ -44,24 +44,25 @@ public class GeneralComponent : ComponentBase
         else
             ToastService.ShowError(await result.Content.ReadAsStringAsync());
     }
-
+    public async Task<bool> ShowDeleteConfirmation(string item)
+        => await ShowConfirm("Delete Confirmation", $"Are you sure to delete this {item}?");
     public async Task<bool> ShowConfirm(string title, string message)
     {
-        var confirm = Modal.Show<ConfirmDialog>(title,
-        new ModalParameters().Add(nameof(ConfirmDialog.Text), message),
+        var confirm = Modal.Show<ConfirmDialog>(title, new ModalParameters().Add(nameof(ConfirmDialog.Text), message),
         new ModalOptions()
         {
             Size = ModalSize.Medium,
             Position = ModalPosition.TopCenter,
             DisableBackgroundCancel = true,
             HideCloseButton = true,
+            AnimationType = ModalAnimationType.PopInOut,
         });
         var result = await confirm.Result;
         return result.Confirmed;
     }
 
     public async Task ShowModal(Type component, string title, ModalParameters? parameters = null, Func<Task>? afterClose = null)
-    {        
+    {
         var modal = Modal.Show(component, title, parameters ?? new ModalParameters().Add(nameof(IsModal), true), ModalOptions);
         await modal.Result;
         if (afterClose is not null)
@@ -70,14 +71,9 @@ public class GeneralComponent : ComponentBase
             await afterClose.Invoke();
             LoaderService.ToggleLoader();
         }
-        else StateHasChanged();
+        else await InvokeAsync(StateHasChanged);
     }
 
-    public void GoTo(string PageRoute, bool WithId = false, Guid? Id = null)
-    {
-        if (WithId)
-            Navigation.NavigateTo($"/{PageRoute}/{Id}");
-        else
-            Navigation.NavigateTo($"/{PageRoute}");
-    }
+    public void GoTo(string PageRoute, Guid? Id = null)
+        => Navigation.NavigateTo($"/{PageRoute}{(Id.HasValue ? "/" + Id : "")}");
 }

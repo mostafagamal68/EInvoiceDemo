1fc73d0c77c56943df0878a7ddd32ad3dd8ea07f
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2024-07-22 20:54:06 +0300

    Add range class and ranger component

---
+++ Shared/Models/Range.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EInvoiceDemo.Shared.DTOs;
+
+public class Range<T>
+{
+    [Range(0, int.MaxValue, ErrorMessage = "Min Value is 0")]
+    public T From { get; set; }
+
+    [Range(0, int.MaxValue, ErrorMessage = "Min Value is 0")]
+    public T To { get; set; }
+}

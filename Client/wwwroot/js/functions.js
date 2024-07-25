b8537b4364d9827cfd84c379ed5a8ceb2fcb786a
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2024-07-25 18:17:58 +0300

    Focus first input element inside form

---
+++ Client/wwwroot/js/functions.js
@@ -0,0 +1,12 @@
+﻿function focusFirstInput(element) {
+    var firstInput = element.querySelector('input');
+    if (firstInput) {
+        firstInput.focus();
+    }
+}
+function focusElement(element) {
+    console.log(element)
+    if (element) {
+        element.focus();
+    }
+}

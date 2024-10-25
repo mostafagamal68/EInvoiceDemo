8281b506aca063cfbda10e5eaabe7bd2e410bdad
Mostafa Gamal <mostafa.gamal_98@hotmail.com>
2024-10-25 23:28:00 +0300

    Add InputDecimal to round decimal numbers after binding value

---
+++ Client/Components/InputDecimal.cs
@@ -0,0 +1,12 @@
+﻿using EInvoiceDemo.Shared.Helpers;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace EInvoiceDemo.Client.Components;
+
+public class InputDecimal : InputNumber<decimal?>
+{
+    protected override string? FormatValueAsString(decimal? value)
+    {
+        return base.FormatValueAsString(value.Rnd());
+    }
+}
